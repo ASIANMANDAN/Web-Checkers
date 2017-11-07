7239f0c3fc45cb83f63f2678e39cce3c0e8652c0
Dan Wang <dcw2772@rit.edu>
2017-11-06 22:23:31 -0500

    Finished Game View Test

--- src/test/java/com/webcheckers/ui/GameViewTest.java
+++ src/test/java/com/webcheckers/ui/GameViewTest.java
@@ -1,7 +1,9 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.appl.CurrentGames;
 import com.webcheckers.model.Player;
 import com.webcheckers.model.board.Board;
+import com.webcheckers.ui.boardView.BoardView;
 import org.junit.Test;
 import spark.ModelAndView;
 import spark.TemplateEngine;
@@ -20,33 +22,65 @@ import static org.mockito.Mockito.mock;
  */
 public class GameViewTest {
 
-    private static final String SIGNOUT_LINK_TAG = "<a href=\"/signout\">Sign out [player]</a>";
     private static final String HOME_LINK_TAG = "<a href=\"/\">My Home</a>";
 
     private final TemplateEngine engine = new FreeMarkerEngine();
 
     /**
-     * Test the
+     * Test the game view renders correctly for the red player.
      */
     @Test
-    public void test_red(){
+    public void test_red() throws Exception {
+        final String SIGNOUT_LINK_TAG = "<a href=\"/signout\">Sign out [player1]</a>";
         final Map<String, Object> vm = new HashMap<>();
         final ModelAndView modelAndView = new ModelAndView(vm, GetGameRoute.VIEW_NAME);
-        Player currentPlayer = new Player("player");
-        Player opponent = new Player("opponent");
-        Board board = mock(Board.class);
+        Player player1 = new Player("player1");
+        Player player2 = new Player("player2");
+        CurrentGames currentGames = new CurrentGames();
+        currentGames.addGame(player1, player2);
+        BoardView board = new BoardView(player1, currentGames);
 
         vm.put("title", "Game");
 
         vm.put(GetGameRoute.MODE_ATTR, Board.ViewMode.PLAY);
-        vm.put(GetGameRoute.CURR_PLAYER, currentPlayer);
-        vm.put(GetGameRoute.RED_PLAYER_ATTR, currentPlayer);
-        vm.put(GetGameRoute.WHITE_PLAYER_ATTR, opponent);
+        vm.put(GetGameRoute.CURR_PLAYER, player1);
+        vm.put(GetGameRoute.RED_PLAYER_ATTR, player1);
+        vm.put(GetGameRoute.WHITE_PLAYER_ATTR, player2);
         vm.put(GetGameRoute.ACTIVE_ATTR, Board.ActiveColor.RED);
         vm.put(GetGameRoute.BOARD_ATTR, board);
 
         final String viewHtml = engine.render(modelAndView);
 
-        assertTrue("Sign out button exists", viewHtml.contains(SIGNOUT_LINK_TAG));
+        assertTrue("Sign out link exists", viewHtml.contains(SIGNOUT_LINK_TAG));
+        assertTrue("My home link exists", viewHtml.contains(HOME_LINK_TAG));
+    }
+
+    /**
+     * Test the game view renders correctly for the white player.
+     */
+    @Test
+    public void test_white() throws Exception {
+        final String SIGNOUT_LINK_TAG = "<a href=\"/signout\">Sign out [player2]</a>";
+        final Map<String, Object> vm = new HashMap<>();
+        final ModelAndView modelAndView = new ModelAndView(vm, GetGameRoute.VIEW_NAME);
+        Player player1 = new Player("player1");
+        Player player2 = new Player("player2");
+        CurrentGames currentGames = new CurrentGames();
+        currentGames.addGame(player1, player2);
+        BoardView board = new BoardView(player2, currentGames);
+
+        vm.put("title", "Game");
+
+        vm.put(GetGameRoute.MODE_ATTR, Board.ViewMode.PLAY);
+        vm.put(GetGameRoute.CURR_PLAYER, player2);
+        vm.put(GetGameRoute.RED_PLAYER_ATTR, player1);
+        vm.put(GetGameRoute.WHITE_PLAYER_ATTR, player2);
+        vm.put(GetGameRoute.ACTIVE_ATTR, Board.ActiveColor.RED);
+        vm.put(GetGameRoute.BOARD_ATTR, board);
+
+        final String viewHtml = engine.render(modelAndView);
+
+        assertTrue("Sign out link exists", viewHtml.contains(SIGNOUT_LINK_TAG));
+        assertTrue("My home link exists", viewHtml.contains(HOME_LINK_TAG));
     }
 }
