7566e35231342f450eb9d7a1a35823e36b57ef7c
Dan Wang <dcw2772@rit.edu>
2017-11-30 04:09:34 -0500

    Finished GetReplayRouteTest

--- src/test/java/com/webcheckers/ui/GetReplayRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetReplayRouteTest.java
@@ -1,6 +1,7 @@
 package com.webcheckers.ui;
 
 import com.webcheckers.appl.Game;
+import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Player;
 import com.webcheckers.model.board.Board;
 import org.junit.Before;
@@ -8,9 +9,9 @@ import org.junit.Test;
 import spark.*;
 
 import java.util.ArrayList;
+import java.util.Map;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -33,8 +34,7 @@ public class GetReplayRouteTest {
     private TemplateEngine engine;
     private Player player;
     private ArrayList completedGames;
-    private Game game;
-    private Board board;
+    private PlayerLobby lobby;
 
     @Before
     public void setup() throws Exception {
@@ -43,7 +43,7 @@ public class GetReplayRouteTest {
         when(request.session()).thenReturn(session);
         player = mock(Player.class);
         completedGames = mock(ArrayList.class);
-        game = mock(Game.class);
+        lobby = mock(PlayerLobby.class);
 
         engine = mock(TemplateEngine.class);
 
@@ -81,16 +81,52 @@ public class GetReplayRouteTest {
      * Test successfully replaying a game.
      */
     @Test
-    public void test_replay(){
+    public void test_replay() throws Exception {
         Response response = mock(Response.class);
         final MyModelAndView myModelView = new MyModelAndView();
         when(engine.render(any(ModelAndView.class))).thenAnswer(MyModelAndView.makeAnswer(myModelView));
+        when(session.attribute(GetReplayRoute.PLAYERLOBBY_KEY)).thenReturn(lobby);
+        Player red = new Player("red");
+        Player white = new Player("white");
+        Game g = new Game(red, white);
+        String selectedMessage = "Another player has challenged you to a game! Return to the Home Page if you wish " +
+                "to connect.";
 
         //Setup test scenario
         when(session.attribute(GetReplayRoute.CURR_PLAYER)).thenReturn(player);
         when(session.attribute(GetReplayRoute.COMPLETED_GAMES)).thenReturn(completedGames);
         when(request.queryParams(GetReplayRoute.COMPLETED_GAME_PARAM)).thenReturn("0");
-        when(session.attribute(GetReplayRoute.REPLAYING_KEY)).thenReturn(game);
-        when(completedGames.get(0)).thenReturn(game);
+        when(session.attribute(GetReplayRoute.REPLAYING_KEY)).thenReturn(g);
+        when(completedGames.get(0)).thenReturn(g);
+        when(request.queryParams(GetReplayRoute.MOVE_INDEX_PARAM)).thenReturn("0");
+        when(session.attribute(GetReplayRoute.PREVIOUSINDEX_KEY)).thenReturn(null);
+        when(lobby.getWatching(player.getUsername())).thenReturn(Boolean.FALSE);
+        when(lobby.getSelected(player.getUsername())).thenReturn(Boolean.TRUE);
+
+        //Invoke the test
+        CuT.handle(request, response);
+
+        //Check the model is a non null Map
+        final Object model = myModelView.model;
+        assertNotNull(model);
+        assertTrue(model instanceof Map);
+
+        //Check all needed view model data is present
+        final Map<String, Object> vm = (Map<String, Object>) model;
+        assertEquals("Replay", vm.get("title"));
+        assertEquals(Board.ViewMode.REPLAY, vm.get(GetReplayRoute.MODE_ATTR));
+        assertEquals(0, vm.get(GetReplayRoute.MOVELISTSIZE_ATTR));
+        assertEquals(0, vm.get(GetReplayRoute.MOVEINDEX_ATTR));
+        assertEquals(player, vm.get(GetReplayRoute.CURR_PLAYER));
+        assertEquals("red", vm.get(GetReplayRoute.RED_PLAYER_ATTR));
+        assertEquals("white", vm.get(GetReplayRoute.WHITE_PLAYER_ATTR));
+        assertEquals(Board.ActiveColor.RED, vm.get(GetReplayRoute.ACTIVE_ATTR));
+        assertEquals(completedGames, vm.get(GetReplayRoute.COMPLETED_GAMES));
+        assertEquals(selectedMessage, vm.get(GetReplayRoute.SELECTED_ATTR));
+        assertNotNull(vm.get(GetReplayRoute.BOARD_ATTR));
+
+        //Player boolean flags updated
+        assertFalse(player.getWatching());
+        assertFalse(lobby.inLobby(player));
     }
 }
